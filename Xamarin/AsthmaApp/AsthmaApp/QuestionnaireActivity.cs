4f0c63ed6cfb9d75730f9afe2bef4fd05aeaeb6f
John H <johnh.dk93@yahoo.dk>
2018-03-23 11:32:26 +0100

    geo

--- Xamarin/AsthmaApp/AsthmaApp/QuestionnaireActivity.cs
+++ Xamarin/AsthmaApp/AsthmaApp/QuestionnaireActivity.cs
@@ -5,10 +5,14 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Locations;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Plugin.Geolocator;
 
 namespace AsthmaApp
 {
@@ -36,7 +40,11 @@ namespace AsthmaApp
 		}
 		private void FinishButton_Click(object sender, EventArgs e)
 		{
-			Finish();
+			var locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 50;
+			var position = locator.GetPositionAsync().Result;
+			Toast.MakeText(this, "Lat: "+position.Latitude + " - Long: "+position.Longitude, ToastLength.Long);
+			//Finish();
 		}
 
 	}
