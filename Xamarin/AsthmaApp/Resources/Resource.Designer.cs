9e152d1b16fafac01608e83009a1f2fd91557d23
John H <johnh.dk93@yahoo.dk>
2018-04-02 10:50:43 +0200

    alarm 1.1

--- Xamarin/AsthmaApp/Resources/Resource.Designer.cs
+++ Xamarin/AsthmaApp/Resources/Resource.Designer.cs
@@ -60,6 +60,9 @@ namespace AsthmaApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int ActivatedText = 2131034116;
+			
 			// aapt resource value: 0x7f050000
 			public const int AlarmListView = 2131034112;
 			
@@ -69,20 +72,23 @@ namespace AsthmaApp
 			// aapt resource value: 0x7f050002
 			public const int MinuteText = 2131034114;
 			
-			// aapt resource value: 0x7f050006
-			public const int alarmbutton = 2131034118;
-			
-			// aapt resource value: 0x7f050007
-			public const int finishbutton = 2131034119;
-			
-			// aapt resource value: 0x7f050004
-			public const int mapbutton = 2131034116;
-			
 			// aapt resource value: 0x7f050003
-			public const int questionnairebutton = 2131034115;
+			public const int SecondText = 2131034115;
+			
+			// aapt resource value: 0x7f050008
+			public const int alarmbutton = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int finishbutton = 2131034121;
+			
+			// aapt resource value: 0x7f050006
+			public const int mapbutton = 2131034118;
 			
 			// aapt resource value: 0x7f050005
-			public const int rewardsbutton = 2131034117;
+			public const int questionnairebutton = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int rewardsbutton = 2131034119;
 			
 			static Id()
 			{
